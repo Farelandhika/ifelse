d7f5e5c910493e97392b2199c3437b67b610b7d4
Farelandhika <f6916545@gmail.com>
2025-12-23 20:47:31 +0700

    Add files via upload

---
+++ pyton.py
@@ -0,0 +1,8 @@
+print("===SELAMAT DATANG DI KETENTUAN PEMBUATAN SIM===")
+
+umur = int(input("Masukkan Umur Anda:"))
+if umur >= 17:
+    print("===ANDA BERHAK MEMBUAT SIM===")
+else:
+    print("===ANDA TIDAK BERHAK MEMBUAT SIM===")
+    print("===TERIMA KASIH===")
