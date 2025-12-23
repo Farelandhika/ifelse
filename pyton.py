546108e6faeca6dddc51133b465656bb5dd73e07
Farelandhika <f6916545@gmail.com>
2025-12-23 20:59:19 +0700

    Refactor greeting and thank you messages

--- pyton.py
+++ pyton.py
@@ -5,4 +5,6 @@ if umur >= 17:
     print("===ANDA BERHAK MEMBUAT SIM===")
 else:
     print("===ANDA TIDAK BERHAK MEMBUAT SIM===")
-    print("===TERIMA KASIH===")
+    
+print("===TERIMA KASIH===")
+
